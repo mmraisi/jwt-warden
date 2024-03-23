4ebc2a1ae082be63022f3c00ebcbf0102a68c016
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-22 23:37:48 -0400

    feat(server): exported package method

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { decode } from './controllers/decode';
+import { sign } from './controllers/sign';
+import { verify } from './controllers/verify';
+
+module.exports = {
+	sign,
+	decode,
+	verify,
+};
