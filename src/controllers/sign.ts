8193b8f8a093e5384c22810a25791a548dfd5b78
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-22 23:21:25 -0400

    docs(sign): added JSDoc comments

--- src/controllers/sign.ts
+++ src/controllers/sign.ts
@@ -1,6 +1,14 @@
 import jwt, { JwtPayload, SignOptions } from 'jsonwebtoken';
 import { JwtError } from '../utils/jwtError';
 
+/**
+ * Signs a payload to generate a JSON Web Token (JWT).
+ * @param payload The payload to be signed.
+ * @param secretOrPrivateKey The secret or private key used for signing.
+ * @param options (Optional) Additional options for signing the token.
+ * @returns The generated JWT token.
+ * @throws JwtError if payload or secretOrPrivateKey is missing.
+ */
 export function sign(payload: JwtPayload, secretOrPrivateKey: string, options?: SignOptions): string {
 	if (!payload) {
 		throw new JwtError('Payload is required');
