993c487586ca8eaba5ae2bbbe0c042afac8bd1dc
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-22 23:21:06 -0400

    docs(decode): added JSDoc comments

--- src/controllers/decode.ts
+++ src/controllers/decode.ts
@@ -1,6 +1,13 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JwtError } from '../utils/jwtError';
 
+/**
+ * Decode JWT token to extract its payload.
+ * @param token The JWT token to decode.
+ * @returns The decoded payload if successful, otherwise null.
+ * @throws JwtError if token is missing or if decoding fails.
+ */
+
 export function decode(token: string): JwtPayload | null {
 	if (!token) {
 		throw new JwtError('Token is required');
