99d1870769ddfbd3d61dcb2b82756b3330d721a6
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-23 04:41:23 -0400

    fix(package version): update package version

--- release.config.js
+++ release.config.js
@@ -3,8 +3,8 @@ module.exports = {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
-		"@semantic-release/git",
 		"@semantic-release/npm",
+		"@semantic-release/git",
 		[
 			"@semantic-release/github",
 			{
