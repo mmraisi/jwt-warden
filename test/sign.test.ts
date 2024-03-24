e4968df438cf824b8544528729d08baae12e9321
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-24 01:46:23 -0400

    test(config): added test config

--- test/sign.test.ts
+++ test/sign.test.ts
@@ -1,23 +1,20 @@
 import { sign } from "../src/controllers/sign";
 import { describe, expect, it } from "vitest";
+import { JWT_SECRET, user_payload } from "./test.config";
 
 describe("sign function", () => {
 	it("should sign a JWT with valid payload and secret key", () => {
-		const payload = { userId: "123456789", role: "admin" };
-		const secretKey = process.env.JWT_SECRET ?? "";
-		const token = sign(payload, secretKey);
+		const token = sign(user_payload, JWT_SECRET);
 		expect(typeof token).toBe("string");
 	});
 
 	it("should throw an error if payload is missing", () => {
-		const secretKey = "some-secret";
 		// @ts-expect-error: Suppress next line
-		expect(() => sign(null, secretKey)).toThrow("Payload is required");
+		expect(() => sign(null, JWT_SECRET)).toThrow("Payload is required");
 	});
 
 	it("should throw an error if secret key is missing", () => {
-		const payload = { userId: "123456789", role: "admin" };
-		expect(() => sign(payload, "")).toThrow("Secret or private key is required");
+		expect(() => sign(user_payload, "")).toThrow("Secret or private key is required");
 	});
 
 	it("should throw an error if payload is missing", () => {
--- test/verify.test.ts
+++ test/verify.test.ts
@@ -1,18 +1,17 @@
 import { verify } from "../src/controllers/verify";
 import { describe, expect, it } from "vitest";
 import { sign } from "../src/controllers/sign";
+import { JWT_SECRET, user_payload } from "./test.config";
 
 describe("verify function", () => {
-	const payload = { userId: "123456789", role: "admin" };
-	const secretKey = process.env.JWT_SECRET ?? "some-secret";
-	const token = sign(payload, secretKey);
+	const token = sign(user_payload, JWT_SECRET);
 	it("should verify a valid JWT token", () => {
-		const verifiedPayload = verify(token, secretKey);
+		const verifiedPayload = verify(token, JWT_SECRET);
 		expect(verifiedPayload).toBeDefined();
 	});
 
 	it("should throw JwtError if token is missing", () => {
-		expect(() => verify("", secretKey)).toThrow("Token is required");
+		expect(() => verify("", JWT_SECRET)).toThrow("Token is required");
 	});
 
 	it("should throw JwtError if secretOrPublicKey is missing", () => {
@@ -21,6 +20,6 @@ describe("verify function", () => {
 
 	it("should throw JwtError invalid error", () => {
 		const token = "valid_jwt_token";
-		expect(() => verify(token, secretKey)).toThrow("jwt malformed");
+		expect(() => verify(token, JWT_SECRET)).toThrow("jwt malformed");
 	});
 });
